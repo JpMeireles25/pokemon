2d1c9b8bc9ad0e6e4c18d913c3b6d72fb909097e
joao-TimWe <joao.meireles@timwe.com>
2021-07-26 10:19:58 +0100

    Added feedback to when user clicks on mark has favourite

--- app/src/main/java/com/example/pokemon/fragments/PokemonDetailsFragment.kt
+++ app/src/main/java/com/example/pokemon/fragments/PokemonDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -44,6 +45,7 @@ class PokemonDetailsFragment : BaseFragment() {
         val favRequest = FavRequest(name = pokemonDetails.name)
         mark_favourite.setOnClickListener {
             context?.sendPostFavouritePokemon(favRequest)
+            Toast.makeText(context, "${pokemonDetails.name} marked has favourite", Toast.LENGTH_SHORT).show()
         }
     }
 
